07bd1b8338bb692f0c49048f7a030a7395809365
Ekaterina Novikova <ekaterina.novikova@telefonica.com>
2021-10-11 14:20:25 +0200

    func removeNote

--- app2.js
+++ app2.js
@@ -4,7 +4,7 @@ const App = {
       title: "Notizenliste",
       placeholder: "Tipp Notizentitel",
       inputValue: "Neue Notiz",
-      notes: ["1. Notiz", "2.Notiz"] // will keep our notes 
+      notes: ["Notiz", "Notiz"] // will keep our notes 
     }
   },
   methods: {
@@ -13,16 +13,46 @@ const App = {
         // wil show how many times func was called + what we typed
       this.inputValue = event.target.value // this - keyword to call data
         // change value to what we typed in input field
-    },
+    }, 
     addNote(){
-      this.notes.push(this.inputValue) // py append
+      if (this.inputValue !== "") {
+        this.notes.push(this.inputValue) // py append
       this.inputValue = "" // to clear input field
+      }
     },
-    inputKeypress(event){
-      // if (event.key === "Enter"()
+    makesUpper(item) {
+      return item.toUpperCase()
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1) // to delete 1 el-t with index
     }
+  },
+  computed: { // vi4eslyaemie // getter setter
+    doubleCount(){ // is a getter
+      return this.notes.length * 2 
+    } 
   }
 }
 
 
 Vue.createApp(App).mount('#app') // connect with html
+
+
+/* 
+1.
+  inputKeypress(event){
+    if (event.key === "Enter") {
+      this.addNote()
+    }
+    v-on:keypress="inputKeypress" in view
+    }
+    or just v-on:keypress.enter="addNote"
+
+2.
+  doubleCount(){
+    console.log("doubleCount")
+    return notes.length * 2 
+  }
+  will be called so many times, how many chars you tipped
+  -> for more productivity we use 'computed:' 
+*/
