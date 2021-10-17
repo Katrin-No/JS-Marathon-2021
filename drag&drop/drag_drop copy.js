9cb6910e73f843c335797c7da08de0bb9e3ad452
Ekaterina Novikova <ekaterina.novikova@telefonica.com>
2021-10-17 12:08:12 +0200

    fixed addTask

--- drag&drop/drag_drop copy.js
+++ drag&drop/drag_drop copy.js
@@ -2,20 +2,21 @@ Vue.createApp({
   data: () => ({
     title: "Manage your tasks",
     placeholder: "New task",
-    inputValue: "New task",
-    tasks: ["Task", "Task2"]
+    inputValue: "",
+    tasks: ["Task", "Vue"]
   }),
   methods: {
     // add&remove task
     addTask(){
       if (this.inputValue !== "") {
-        this.tasks.push(inputValue)
-        this.inputValue = "" // to clear input field
+        this.tasks.push(this.inputValue) // py append
+      this.inputValue = "" // to clear input field
       }
     },
     removeTask(index) {
       this.tasks.splice(index, 1) // to delete 1 el-t with index
     },
+
     // drag&drop
     dragStart(event) {
       event.target.classList.add('hold') // our el-t dissapears, thats why we need delay
@@ -23,14 +24,17 @@ Vue.createApp({
       // el-t dissapears only in to-do col
     },
     dragEnd(event) {
+      console.log ("drag end", event.target)
       event.target.classList.remove('hold', 'hide') // or ->
       // event.target.className = "item"
     },
+
     dragOver(event) { 
       event.preventDefault() // el-t doesn't come back in to-do
     },
     dragEnter(event) {
       event.target.classList.add('hovered')
+      console.log ("drag enter", event.target)
     }, 
     dragLeave(event) {
       event.target.classList.remove('hovered')
@@ -38,6 +42,14 @@ Vue.createApp({
     dragDrop(event) {
       event.target.classList.remove('hovered')
       event.target.append(document.querySelector(".item")) // el-t stays in new placeholder
+    }
+  },
+  watch: { // we can monitor changes in variables
+    inputValue(value) {
+      if (value.length > 10) { // validation
+        this.inputValue = ""
+      }
+      console.log("inputValue changed", value)
     }
   }
 }).mount('#app')
